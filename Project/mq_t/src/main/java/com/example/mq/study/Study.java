0196d1c921a102bdf45e56be8bfe11aecdf8898c
zhang-haoluck <1210931886@qq.com>
2024-08-03 23:37:47 +0800

    8.3

--- Project/mq_t/src/main/java/com/example/mq/study/Study.java
+++ Project/mq_t/src/main/java/com/example/mq/study/Study.java
@@ -1,29 +1,78 @@
 package com.example.mq.study;
 
-import com.example.mq.mqserver.BrokerServer;
-import com.example.mq.mqserver.VirtualHost;
+import com.example.mq.common.MqException;
+import com.example.mq.mqserver.core.*;
+import com.example.mq.mqserver.datacenter.DiskDataCenter;
+import com.example.mq.mqserver.datacenter.MemoryDataCenter;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
+import java.util.Map;
 
 public class Study {
     
-    private ServerSocket serverSocket = null;
+    private String virtualHostName;
+    private MemoryDataCenter memoryDataCenter = new MemoryDataCenter();
+    private DiskDataCenter diskDataCenter = new DiskDataCenter();
     
-    private VirtualHost virtualHost = new VirtualHost("default");
+    private Router router = new Router();
     
-    private ConcurrentHashMap<String, Socket> sessions = new ConcurrentHashMap<>();
-    
-    private ExecutorService executorService = null;
-    
-    public BrokerServer(int port) {
-        serverSocket = new ServerSocket(port);
+    private ConsumerManager consumerManager = new ConsumerManager();
+    private final Object exchangeLocker = new Object();
+
+    public String getVirtualHostName() {
+        return virtualHostName;
+    }
+
+    public MemoryDataCenter getMemoryDataCenter() {
+        return memoryDataCenter;
+    }
+
+    public DiskDataCenter getDiskDataCenter() {
+        return diskDataCenter;
     }
     
-    public void start() {
-        
+    public boolean exchangeDelete(String exchangeName) {
+        exchangeName = virtualHostName + exchangeName;
+        try {
+            synchronized (exchangeLocker) {
+                Exchange toDelete = memoryDataCenter.getExchange(exchangeName);
+                if (toDelete == null) {
+                    throw new MqException("")
+                }
+            }
+        }
+    }
+    
+    public boolean basicPublish(String exchangeName, String routingKey, BasicProperties basicProperties, byte[] body) {
+        try {
+            exchangeName = virtualHostName + exchangeName;
+            if (!router.checkRoutingKey(routingKey)) {
+                throw new MqException("[VirtualHost] routingKey 非法! routingKey=" + routingKey);
+            }
+            Exchange exchange = memoryDataCenter.getExchange(exchangeName);
+            if (exchange == null) {
+                throw new MqException("")
+            }
+        } catch (MqException e) {
+            throw new RuntimeException(e);
+        }
+
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
