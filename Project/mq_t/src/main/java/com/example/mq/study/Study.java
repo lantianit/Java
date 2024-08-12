ec3c96622c6229be85133c3f68eece6fd0e8ac60
zhang-haoluck <1210931886@qq.com>
2024-08-12 23:47:05 +0800

    12

--- Project/mq_t/src/main/java/com/example/mq/study/Study.java
+++ Project/mq_t/src/main/java/com/example/mq/study/Study.java
@@ -1,110 +1,140 @@
 package com.example.mq.study;
 
+import com.example.mq.common.ConsumerEnv;
 import com.example.mq.common.MqException;
+import com.example.mq.mqserver.VirtualHost;
 import com.example.mq.mqserver.core.*;
 import com.example.mq.mqserver.datacenter.DiskDataCenter;
 import com.example.mq.mqserver.datacenter.MemoryDataCenter;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Study {
     
     private String virtualHostName;
+    
     private MemoryDataCenter memoryDataCenter = new MemoryDataCenter();
+    
     private DiskDataCenter diskDataCenter = new DiskDataCenter();
     private Router router = new Router();
     private ConsumerManager consumerManager = new ConsumerManager();
     
     private final Object exchangeLocker = new Object();
     private final Object queueLocker = new Object();
-
+    
     public String getVirtualHostName() {
         return virtualHostName;
     }
-
+    
     public MemoryDataCenter getMemoryDataCenter() {
         return memoryDataCenter;
     }
-
+    
     public DiskDataCenter getDiskDataCenter() {
         return diskDataCenter;
     }
     
-    public Study(String name) throws RuntimeException {
-        this.virtualHostName = name;
+    public Study(String queueName) {
+        this.virtualHostName = queueName;
         diskDataCenter.init();
         try {
             memoryDataCenter.recovery(diskDataCenter);
         } catch (IOException | MqException | ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("");
+            throw new RuntimeException(e);
         }
+    }
+    
+}
+
+class ConsumerManager {
+    
+    private VirtualHost parent;
+    private ExecutorService workerPool = Executors.newFixedThreadPool(4);
+    private BlockingQueue<String> tokenQueue = new LinkedBlockingQueue<>();
+    private Thread scannerThread = null;
+    
+    public c(VirtualHost p) {
+        parent = p;
+        scannerThread = new Thread(() -> {
+           while (true) {
+               try {
+                   String queueName = tokenQueue.take();
+                   MSGQueue queue = parent.getMemoryDataCenter().getQueue(queueName);
+                   synchronized (queue) {
+                       consumeMessage(queue);
+                   }
+               } catch (InterruptedException e) {
+                   throw new RuntimeException(e);
+               }
+           }
+        });
+        
     }
 
-    public boolean exchangeDeclare(String exchangeName, ExchangeType exchangeType, boolean durable, boolean autoDelete,
-                                   Map<String, Object> arguments) {
-        // 把交换机的名字, 加上虚拟主机作为前缀.
-        exchangeName = virtualHostName + exchangeName;
-        try {
-            synchronized (exchangeLocker) {
-                // 1. 判定该交换机是否已经存在. 直接通过内存查询.
-                Exchange existsExchange = memoryDataCenter.getExchange(exchangeName);
-                if (existsExchange != null) {
-                    // 该交换机已经存在!
-                    System.out.println("[VirtualHost] 交换机已经存在! exchangeName=" + exchangeName);
-                    return true;
-                }
-                // 2. 真正创建交换机. 先构造 Exchange 对象
-                Exchange exchange = new Exchange();
-                exchange.setName(exchangeName);
-                exchange.setType(exchangeType);
-                exchange.setDurable(durable);
-                exchange.setAutoDelete(autoDelete);
-                exchange.setArguments(arguments);
-                // 3. 把交换机对象写入硬盘
-                if (durable) {
-                    diskDataCenter.insertExchange(exchange);
-                }
-                // 4. 把交换机对象写入内存
-                memoryDataCenter.insertExchange(exchange);
-                System.out.println("[VirtualHost] 交换机创建完成! exchangeName=" + exchangeName);
-                // 上述逻辑, 先写硬盘, 后写内存. 目的就是因为硬盘更容易写失败. 如果硬盘写失败了, 内存就不写了.
-                // 要是先写内存, 内存写成功了, 硬盘写失败了, 还需要把内存的数据给再删掉. 就比较麻烦了.
-            }
-            return true;
-        } catch (Exception e) {
-            System.out.println("[VirtualHost] 交换机创建失败! exchangeName=" + exchangeName);
-            e.printStackTrace();
-            return false;
+    private void consumeMessage(MSGQueue queue) {
+
+        ConsumerEnv luckDog = queue.chooseConsumer();
+        if (luckDog == null) {
+            return;
         }
-    }
-    
-    public boolean exchangeDelete(String exchangeName) {
-        synchronized (exchangeLocker) {
-            // 1. 先找到对应的交换机.
-            Exchange toDelete = memoryDataCenter.getExchange(exchangeName);
-            if (toDelete == null) {
-                try {
-                    throw new MqException("[VirtualHost] 交换机不存在无法删除!");
-                } catch (MqException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            // 2. 删除硬盘上的数据
-            if (toDelete.isDurable()) {
-                diskDataCenter.deleteExchange(exchangeName);
-            }
-            // 3. 删除内存中的交换机数据
-            memoryDataCenter.deleteExchange(exchangeName);
-            System.out.println("[VirtualHost] 交换机删除成功! exchangeName=" + exchangeName);
+        Message message = parent.getMemoryDataCenter().pollMessage(queue.getName());
+        if (message == null) {
+            return;
         }
-        return true;
+        workerPool.submit(() -> {
+            try {
+                parent.getMemoryDataCenter().addMessageWaitAck(queue.getName(), message);
+                luckDog.getConsumer().handleDelivery(luckDog.getConsumerTag(), message.getBasicProperties(), message.getBody());
+                
+            }
+        })
     }
+
+}
+
+class ConsumerManager2 {
+    private VirtualHost parent;
+    private ExecutorService wokrerPool = Executors.newFixedThreadPool(4);
+    private BlockingQueue<String> tokenQueue = new LinkedBlockingQueue<>();
+    private Thread scannerThread = null;
     
-    public boolean basicAck(String queueName, String messageId) {
-        queueName = virtualHostName + queueName;
-    }
-    
+    public ConsumerManager2(VirtualHost p) {
+        parent = p;
         
+        scannerThread = new Thread(() -> {
+           while (true) {
+               try {
+                   String queueName = tokenQueue.take();
+                   MSGQueue queue = parent.getMemoryDataCenter().getQueue(queueName);
+                   if (queue == null) {
+                       throw new MqException("");
+                   }
+                   synchronized (queue) {
+                       consumeMessage(queue);
+                   }
+               } catch (InterruptedException | MqException e) {
+                   throw new RuntimeException(e);
+               }
+           }
+        });
+        
+    }
+
+    private void consumeMessage(MSGQueue queue) {
+        ConsumerEnv luckyDog = queue.chooseConsumer();
+        if (luckyDog == null) {
+            return;
+        }
+        Message message = parent.getMemoryDataCenter().pollMessage(queue.getName());
+        if (message == null) {
+            return;
+        }
+        
+    }
+
+
 }
