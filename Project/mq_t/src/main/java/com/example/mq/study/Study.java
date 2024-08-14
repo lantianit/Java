035ba466fb9a8982a77df7154508da98a8217692
zhang-haoluck <1210931886@qq.com>
2024-08-14 23:10:08 +0800

    14

--- Project/mq_t/src/main/java/com/example/mq/study/Study.java
+++ Project/mq_t/src/main/java/com/example/mq/study/Study.java
@@ -1,140 +1,38 @@
 package com.example.mq.study;
 
-import com.example.mq.common.ConsumerEnv;
-import com.example.mq.common.MqException;
 import com.example.mq.mqserver.VirtualHost;
-import com.example.mq.mqserver.core.*;
-import com.example.mq.mqserver.datacenter.DiskDataCenter;
-import com.example.mq.mqserver.datacenter.MemoryDataCenter;
 
 import java.io.IOException;
-import java.util.concurrent.BlockingQueue;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class Study {
     
-    private String virtualHostName;
+    private ServerSocket serverSocket = null;
     
-    private MemoryDataCenter memoryDataCenter = new MemoryDataCenter();
+    private VirtualHost virtualHost = new VirtualHost("default");
+    private ConcurrentHashMap<String, Socket> sessions = new ConcurrentHashMap<>();
+    private ExecutorService executorService = null;
+    private volatile boolean runnable = true;
     
-    private DiskDataCenter diskDataCenter = new DiskDataCenter();
-    private Router router = new Router();
-    private ConsumerManager consumerManager = new ConsumerManager();
-    
-    private final Object exchangeLocker = new Object();
-    private final Object queueLocker = new Object();
-    
-    public String getVirtualHostName() {
-        return virtualHostName;
+    public Study(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
     }
     
-    public MemoryDataCenter getMemoryDataCenter() {
-        return memoryDataCenter;
-    }
-    
-    public DiskDataCenter getDiskDataCenter() {
-        return diskDataCenter;
-    }
-    
-    public Study(String queueName) {
-        this.virtualHostName = queueName;
-        diskDataCenter.init();
+    public void start() {
+        System.out.println("[BrokerServer] 启动！");
+        executorService = Executors.newCachedThreadPool();
         try {
-            memoryDataCenter.recovery(diskDataCenter);
-        } catch (IOException | MqException | ClassNotFoundException e) {
+            while (runnable) {
+                Socket clientSocket = serverSocket.accept();
+                
+            }
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
     
-}
-
-class ConsumerManager {
-    
-    private VirtualHost parent;
-    private ExecutorService workerPool = Executors.newFixedThreadPool(4);
-    private BlockingQueue<String> tokenQueue = new LinkedBlockingQueue<>();
-    private Thread scannerThread = null;
-    
-    public c(VirtualHost p) {
-        parent = p;
-        scannerThread = new Thread(() -> {
-           while (true) {
-               try {
-                   String queueName = tokenQueue.take();
-                   MSGQueue queue = parent.getMemoryDataCenter().getQueue(queueName);
-                   synchronized (queue) {
-                       consumeMessage(queue);
-                   }
-               } catch (InterruptedException e) {
-                   throw new RuntimeException(e);
-               }
-           }
-        });
-        
-    }
-
-    private void consumeMessage(MSGQueue queue) {
-
-        ConsumerEnv luckDog = queue.chooseConsumer();
-        if (luckDog == null) {
-            return;
-        }
-        Message message = parent.getMemoryDataCenter().pollMessage(queue.getName());
-        if (message == null) {
-            return;
-        }
-        workerPool.submit(() -> {
-            try {
-                parent.getMemoryDataCenter().addMessageWaitAck(queue.getName(), message);
-                luckDog.getConsumer().handleDelivery(luckDog.getConsumerTag(), message.getBasicProperties(), message.getBody());
-                
-            }
-        })
-    }
-
-}
-
-class ConsumerManager2 {
-    private VirtualHost parent;
-    private ExecutorService wokrerPool = Executors.newFixedThreadPool(4);
-    private BlockingQueue<String> tokenQueue = new LinkedBlockingQueue<>();
-    private Thread scannerThread = null;
-    
-    public ConsumerManager2(VirtualHost p) {
-        parent = p;
-        
-        scannerThread = new Thread(() -> {
-           while (true) {
-               try {
-                   String queueName = tokenQueue.take();
-                   MSGQueue queue = parent.getMemoryDataCenter().getQueue(queueName);
-                   if (queue == null) {
-                       throw new MqException("");
-                   }
-                   synchronized (queue) {
-                       consumeMessage(queue);
-                   }
-               } catch (InterruptedException | MqException e) {
-                   throw new RuntimeException(e);
-               }
-           }
-        });
-        
-    }
-
-    private void consumeMessage(MSGQueue queue) {
-        ConsumerEnv luckyDog = queue.chooseConsumer();
-        if (luckyDog == null) {
-            return;
-        }
-        Message message = parent.getMemoryDataCenter().pollMessage(queue.getName());
-        if (message == null) {
-            return;
-        }
-        
-    }
-
-
 }
